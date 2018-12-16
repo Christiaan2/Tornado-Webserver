5c4454a223fac134780f30894202ad86d21fca47
Christiaan2 <christiaanreurslag@gmail.com>
2018-12-16 01:42:52 +0100

    Create IOHandler class to handle the I/O

--- Tornado_HelloWorld.py
+++ Tornado_HelloWorld.py
@@ -6,6 +6,7 @@ import socket
 import json
 import numpy as np
 import time
+import sys
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -17,12 +18,10 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
     def open(self):
         self.clients.add(self)
         print('[WS] New connection, ',len(self.clients),' client(s)')
-        self.write_message(json.dumps(data_json))
+        self.write_message(IOHandler.__str__())
 
     def on_message(self, message):
-        m_json = json.loads(message)
-        index = int(m_json["Name"][4:])
-        data_json["DOut"][index-8] = m_json["Value"]
+        IOHandler.updateOutput(message)
         self.update_clients()
     
     def on_close(self):
@@ -31,7 +30,39 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
 
     @classmethod
     def update_clients(cls):
-        [client.write_message(json.dumps(data_json)) for client in cls.clients]
+        [client.write_message(IOHandler.__str__()) for client in cls.clients]
+
+class IOHandler():
+    def __init__(self):
+        sys.exit('ERROR: IOHandler instance should NOT be created')
+    
+    @classmethod
+    def initialize(cls):
+        with open('InitialInputs.json') as file:
+            cls.data_json = json.load(file)
+        cls.prev_time = time.time()
+        tornado.ioloop.PeriodicCallback(cls.updateJson,5000).start()
+    
+    @classmethod
+    def updateJson(cls):
+        cur_time = time.time()
+        print("Update json, elapsed time: ", cur_time-cls.prev_time)
+        cls.prev_time = cur_time
+        for i in range(len(cls.data_json["AIn"])):
+            cls.data_json["AIn"][i] = np.round(5*np.random.random(),5)
+        for i in range(len(cls.data_json["DIn"])):
+            cls.data_json["DIn"][i] = np.random.random() > 0.5
+        WebSocketHandler.update_clients()
+
+    @classmethod
+    def updateOutput(cls, message):
+        m_json = json.loads(message)
+        index = int(m_json["Name"][4:])
+        cls.data_json["DOut"][index-8] = m_json["Value"]
+    
+    @classmethod
+    def __str__(cls):
+        return json.dumps(cls.data_json)
 
 def make_app():
     settings = {
@@ -44,21 +75,11 @@ def make_app():
     ]
     return tornado.web.Application(handlers, **settings)
 
-def updateJson():
-    print("Update json, elapsed time: ", time.time())
-    for i in range(len(data_json["AIn"])):
-        data_json["AIn"][i] = np.round(5*np.random.random(),5)
-    for i in range(len(data_json["DIn"])):
-        data_json["DIn"][i] = np.random.random() > 0.5
-    WebSocketHandler.update_clients()
-
 if __name__ == "__main__":
-    with open('InitialInputs.json') as file:
-        data_json = json.load(file) #Global variable
-
     app = make_app()
     app.listen(8888)
-    tornado.ioloop.PeriodicCallback(updateJson,5000).start()
+
+    IOHandler.initialize()
 
     print('Webserver started')
     print('Ip adress of server: ' + socket.gethostbyname(socket.gethostname()))
